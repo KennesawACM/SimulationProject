5efba62fca80d6125574b7856c64aff3e24bb7bd
mr7657 <mr7657@Marcuss-MacBook-Pro.local>
2015-02-04 14:23:23 -0500

    This is dumb
    Test

---
+++ Simulator/Simulator/src/simulator/Test.java
@@ -0,0 +1,13 @@
+package simulator;
+
+public class Test {
+	
+	//Main Class
+	public static void main(String[] args) {
+		//Test();
+	}
+	
+	public Test() {
+		//this is a constructor for test
+	}
+}
