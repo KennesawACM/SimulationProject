2af2b29dfaf7be70370ce138c1706994477292af
1Ariadna1 <ariadna.alpizar@gmail.com>
2015-02-04 00:32:20 -0500

    Added some more to person class and to the run class to show stuff from the person class.

--- Simulator/src/log/Message.java
+++ Simulator/src/log/Message.java
@@ -2,7 +2,10 @@ package log;
 
 public class Message extends Logger {
 
-	public Message() {
+	public Message() 
+	{
+		
+		
 		
 	}
 	
--- Simulator/src/simulator/Run.java
+++ Simulator/src/simulator/Run.java
@@ -1,5 +1,7 @@
 package simulator;
 
+import simulator.Person.Country;
+
 /**
  * @author Tyler Davis
  * This will initiate the Simulator
@@ -16,5 +18,27 @@ public class Run extends Simulator {
 		Simulator sim = getInstance(Simulator_Type.DISEASE_TWO);
 		System.out.println("Sim type: " + sim.get_Type());
 		sim.run_Simulator();
+		
+		/* Added a person and some characteristics
+		 * 
+		 * Future Thoughts: 
+		 * 
+		 * 
+		 * Attribute Logic/Calculations to the Degree Of Exposure Based
+		 * Add something to the Person Class that would determine the 
+		 * likelihood of being contaminated within a certain period (Month, Day, Second, etc.)
+		 */
+		Person p = new Person();
+		p.setAge(90);
+		p.setInfectedStatus(true);
+		p.setSex("Male");
+		p.setDiseaseType(sim.get_Type());
+		p.setCountryOfOrigin(Country.GUINEA);
+		p.setDegreeOfExposure(0.98);
+		
+		System.out.println(p.toString());
+		
+
+		
 	}
 }
