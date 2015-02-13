e713df31e57bd62edc3974581c879557cdd14627
JasonBradley <jasonbdly@gmail.com>
2015-02-13 16:51:59 -0500

    Added Test Log Implementations

--- Simulator/src/log/OtherLog.java
+++ Simulator/src/log/OtherLog.java
@@ -7,6 +7,7 @@ public class OtherLog extends Log {
     @Override
     public void debug(Level level, String message) {
         //TODO: Handle specific implementation of this
+        //i.e. Add log line to file or file buffer
         System.out.println(super.generateLog(level, message));
     }
 }
