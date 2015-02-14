f3587762ece2a3a1af581708fa1be331a67c1ea4
mr7657 <mr7657@Marcuss-MacBook-Pro.local>
2015-02-13 21:57:43 -0500

    Lets Get Started
    I know there was a lot of confusion today so I am rolling back the
    clock. I have created some basic structures, that we will be using in
    the backend. Take a look at it and let me know if you have questions.

--- Simulator/src/log/Error_Log.java
+++
@@ -1,5 +0,0 @@
-package log;
-// Here, I finally made a change: Mike
-public class Error_Log {
-
-}
---
+++ Simulator/src/log/UserLog.java
@@ -0,0 +1,59 @@
+package log;
+
+//Only Should be accessed by Logger class
+//User Logs are here
+class UserLog {
+	//Private Variables 
+	private int LogLevel;
+	private String Message, SysTime, MessageID;
+	
+	/**
+	 * @author mr7657
+	 * @category Constructor
+	 * @param level
+	 * @param message
+	 * @param time
+	 */
+	public UserLog(int level, String message, String time){
+		this.LogLevel = level;
+		this.Message = message;
+		this.SysTime = time;
+		
+		//Choose Log Level
+		if(ChooseLevel())
+			//For now Print to con
+			System.out.println("Message Logged, Message ID: " + this.MessageID);
+		else{
+			//false Failed
+			System.out.println("Error, Message Not Logged, Please Contact the Backend Team");
+			
+			//Throw Exception
+		}
+	}
+	
+	/**
+	 * @author mr7657
+	 * @param None
+	 * @summary This will check which log level and then calls that method
+	 * @return Boolean: Valid Level
+	 */
+	private boolean ChooseLevel(){
+		boolean returnValue = false;
+		switch(this.LogLevel){
+		case 1:
+			//Debug
+		case 2:
+			//Info
+		case 3:
+			//Warning
+		case 4:
+			//Error
+		case 5:
+			//Fatal
+		case 6:
+			//Off
+		}
+		return returnValue;
+	}
+
+}
